c57f844bd2b42947b080b3145d8cdf5e3384d79b
Hyun Bin Yim <hby0818@gmail.com>
2023-01-24 10:44:36 -0500

    htmlcsspartial

---
+++ leaguefour.js
@@ -0,0 +1,15 @@
+//Theme is League of Legends
+
+//make a function that will make the html/css appear visible after the win condition is met aka (Player 1 Wins!)
+//make a function that will make the html/css appear visible affter the win condition is met aka (Player 2 Wins!)
+//make a function that will make the html/css appear after there are no possible positions for the pieces to be put aka (Draw!) 
+//make a function that will make the css appear after the Draw textbox is shown, stating (Play Again?)
+//make a function that will reset the game after pressing yes to Play Again? or pressing the reset button
+//create a reset button that resets the game
+//make a way that will have a textbox that states whose turn it is aka (Player 1's Turn)
+//make a way that will have a textbox that states whose turn it is aka (Player 2's Turn)
+//make a win condition for Player 1
+//make a win condition for Player 2
+//make a draw condition if there are no more applicable pieces
+//make it so you cant place pieces after game is finished
+//when clicking on board, make the first player visible
