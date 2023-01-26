45a6e898d14181698e146f51bdff337fcda0c8fa
Hyun Bin Yim <hby0818@gmail.com>
2023-01-25 22:02:22 -0500

    names

--- leaguefour.js
+++ leaguefour.js
@@ -1,19 +1,9 @@
-//make a function that will make the html/css appear visible after the win condition is met aka (Player 1 Wins!)
-//make a function that will make the html/css appear visible affter the win condition is met aka (Player 2 Wins!)
-//make a function that will make the html/css appear after there are no possible positions for the pieces to be put aka (Draw!) 
-//make a function that will make the css appear after the Draw textbox is shown, stating (Play Again?)
-//make a function that will reset the game after pressing yes to Play Again? or pressing the reset button
-//create a reset button that resets the game
-//make a way that will have a textbox that states whose turn it is aka (Player 1's Turn)
-//make a way that will have a textbox that states whose turn it is aka (Player 2's Turn)
-//make a win condition for Player 1
-//make a win condition for Player 2
-//make a draw condition if there are no more applicable pieces
-//make it so you cant place pieces after game is finished
-const circles = document.querySelectorAll(".circle")
 let currentPlayer = "porored"
 let redWins = false
 let whiteWins = false
+const whichPlayer = document.querySelector(".whichPlayer")
+const circles = document.querySelectorAll(".circle")
+whichPlayer.innerText = "Red Poro's Turn"
 const redMoves = []
 const whiteMoves = []
 const checkWin = () => {
@@ -30,25 +20,28 @@ const checkWin = () => {
 }
 
 const placeCircle = (e) => {
-
     if (e.target.classList.length === 1) {
         if (currentPlayer === "porored") {
             e.target.classList.add("porored")
+            whichPlayer.innerText = "White Poro Turn"
             redMoves.push(parseInt(e.target.innerText))
             checkWin() 
             console.log(redWins)
             if (redWins === true) {
-                alert ('red wins')
-
+                whichPlayer.innerText = "Red Poro Wins"
+                //if red poro wins, make every circle unclickable
+                restartFunction()
             }
             currentPlayer = "porowhite"
         } else if (currentPlayer === "porowhite") {
             e.target.classList.add("porowhite")
+            whichPlayer.innerText = "Red Poro's Turn"
             whiteMoves.push(parseInt(e.target.innerText))
             checkWin()
             console.log(whiteWins)
             if (whiteWins === true) {
-                alert ('white wins')
+                whichPlayer.innerText = "White Poro Wins"
+                restartFunction()
             }
             currentPlayer = "porored"
         }
